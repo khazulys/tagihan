92c01599ac20bc47f5f7d4e33489c5db112a43ba
khazulys <khazulyusserys@gmail.com>
2025-11-08 01:30:12 +0000

    fix: use compatible versions - Compose 1.6.11 with Kotlin 2.0.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,8 @@ pluginManagement {
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     plugins {
-        id("org.jetbrains.compose") version "1.7.0"
-        kotlin("multiplatform") version "2.0.21"
+        id("org.jetbrains.compose") version "1.6.11"
+        kotlin("multiplatform") version "2.0.0"
         id("com.android.application") version "8.2.2"
     }
 }
